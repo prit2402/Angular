97e31a9b9bf1b854f2baa084804f0f789d416dfd
priti2402 <priti.kokare@cumminscollege.in>
2023-06-28 16:01:16 +0530

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ArchivesComponent } from './pages/archives/archives.component';
+import { ListComponent } from './pages/list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:HomeComponent},
+  { path: 'archives', component:ArchivesComponent},
+  { path: 'list', component:ListComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './sharepage/navbar/navbar.component';
+import { FooterComponent } from './sharepage/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ArchivesComponent } from './pages/archives/archives.component';
+import { ListComponent } from './pages/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    ArchivesComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
